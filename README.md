a9703fed27bf5359c08f8ff35ce020288c21a0db
asuran <bopebaqytkeldy@gmail.com>
2025-10-04 09:22:06 +0500

    init: maven project structure, ci setup

---
+++ README.md
@@ -0,0 +1,3 @@
+Max-Heap implementation - Assignment 2
+Bakytkeldy Akbope Se-2426
+(in pair with Balmakhanov Danial)
---
+++ src/test/java/algorithms/MaxHeapTest.java
@@ -0,0 +1,11 @@
+package algorithms;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MaxHeapTest {
+        @Test
+        void sampleTest() {
+            assertEquals(2, 1 + 1);
+        }
+
+}
